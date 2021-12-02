197a55017ac4c3bb3a4c273f62c8a3d567d5471a
DeZMonD251 <83780168+DeZMonD251@users.noreply.github.com>
2021-12-02 11:51:10 +0200

    Update Server.cpp

--- Lr_5/Server/Server/Server.cpp
+++ Lr_5/Server/Server/Server.cpp
@@ -140,48 +140,6 @@ DWORD WINAPI execute_command(LPVOID lpParam) {
 }
 
 int main(int argc, char* argv[]) {
-    /*
-    
-    char buff[1024];
-    setlocale(LC_ALL, "Rus");
-    string command_client, send_command;
-    printf("UDP DEMO echo_Server \n");
-    if (WSAStartup(0x202, (WSADATA*)&buff[0]))
-    {
-        printf("WSAStartup error : %d \n", WSAGetLastError());
-        return -1;
-    }
-    SOCKET my_sock;
-    my_sock = socket(AF_INET, SOCK_DGRAM, 0);
-    if (my_sock == INVALID_SOCKET)
-    {
-        printf("Socket() error: %d \n", WSAGetLastError());
-        WSACleanup();
-        return -1;
-    }
-    sockaddr_in local_addr;
-    local_addr.sin_family = AF_INET;
-    local_addr.sin_addr.s_addr = INADDR_ANY;
-    local_addr.sin_port = htons(PORT);
-    if (bind(my_sock, (sockaddr*)&local_addr, sizeof(local_addr)))
-    {
-        printf("bind error : %d \n", WSAGetLastError());
-        closesocket(my_sock);
-        WSACleanup();
-        return -1;
-    }
-    while (1) {
-        sockaddr_in client_addr;
-        int client_addr_size = sizeof(client_addr);
-        int bsize = recvfrom(my_sock, &buff[0], sizeof(buff) - 1, 0, (sockaddr*)&client_addr, &client_addr_size);
-         = string(buff);
-        
-        if (bsize == SOCKET_ERROR)
-            printf("recvfrom() error: %d \n", WSAGetLastError());
-        HOSTENT* hst;
-        hst = gethostbyaddr((char*)&client_addr.sin_addr, 4, AF_INET);
-    }
-    */
     DATA d;
     datA = &d;
     DWORD dwThreadId, dwThrdParam = 1;
